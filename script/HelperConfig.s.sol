524ced78ba42fc74492a2ecba5c365597674fc9f
Shahzad Mughal <smughal55@users.noreply.github.com>
2023-06-29 14:39:55 +0100

    chore: goerli config

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -24,20 +24,20 @@ contract HelperConfig is Script {
     uint256 public DEFAULT_ANVIL_PRIVATE_KEY = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
 
     constructor() {
-        if (block.chainid == 11155111) {
-            activeNetworkConfig = getSepoliaEthConfig();
+        if (block.chainid == 5) {
+            activeNetworkConfig = getGoerliEthConfig();
         } else {
             activeNetworkConfig = getOrCreateAnvilEthConfig();
         }
     }
 
-    function getSepoliaEthConfig() public view returns (NetworkConfig memory sepoliaNetworkConfig) {
-        sepoliaNetworkConfig = NetworkConfig({
-            wethUsdPriceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306, // ETH / USD
+    function getGoerliEthConfig() public view returns (NetworkConfig memory goerliNetworkConfig) {
+        goerliNetworkConfig = NetworkConfig({
+            wethUsdPriceFeed: 0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e, // ETH / USD
             wbtcUsdPriceFeed: 0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43,
             weth: 0xdd13E55209Fd76AfE204dBda4007C227904f0a81,
-            wbtc: 0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063,
-            usdc: 0x4DBCdF9B62e891a7cec5A2568C3F4FAF9E8Abe2b,
+            wbtc: 0xA39434A63A52E749F02807ae27335515BA4b07F7,
+            usdc: 0xd35CCeEAD182dcee0F148EbaC9447DA2c4D449c4,
             deployerKey: vm.envUint("PRIVATE_KEY")
         });
     }
